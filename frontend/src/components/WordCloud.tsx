131324059e4d22a1c34a11375a58d865a9817b25
Yoon, Seohee <syoon333@gatech.edu>
2024-11-22 22:38:59 -0500

    added posts and put word cloud into one tap

--- frontend/src/components/WordCloud.tsx
+++ frontend/src/components/WordCloud.tsx
@@ -1,43 +1,45 @@
-import React from "react";
-import { Box } from "@chakra-ui/react";
-import WordCloud from "react-d3-cloud";
+//Integrated this file into AnalyticsReport.tsx . not used anymore
 
-interface Word {
-	text: string;
-	value: number;
-	color?: string;
-	rotate?: number;
-}
+// import React from "react";
+// import { Box } from "@chakra-ui/react";
+// import WordCloud from "react-d3-cloud";
 
-export interface Word3DCloudProps {
-	words: Word[];
-	width?: number;
-	height?: number;
-	onWordSelect?: (word: string) => void; // Callback for word selection
-}
+// interface Word {
+// 	text: string;
+// 	value: number;
+// 	color?: string;
+// 	rotate?: number;
+// }
 
-const Word3DCloud = ({ words, width, height, onWordSelect }: Word3DCloudProps) => {
-	// Define the onWordClick handler
-	const handleWordClick = (word) => {
-		if (onWordSelect) {
-			onWordSelect(word.text); // Call the callback with the selected word text
-		}
-	};
+// export interface Word3DCloudProps {
+// 	words: Word[];
+// 	width?: number;
+// 	height?: number;
+// 	onWordSelect?: (word: string) => void; // Callback for word selection
+// }
 
-	return (
-		<Box borderRadius="lg" p={4} border={"1px solid"} borderColor={"gray.200"}>
-			<WordCloud
-				data={words}
-				width={1500}
-				height={height}
-				font="arial"
-				fontSize={(d) => d.value}
-				padding={5}
-				rotate={(d) => d.rotate || 0}
-				onWordClick={handleWordClick} // Set the click handler
-			/>
-		</Box>
-	);
-};
+// const Word3DCloud = ({ words, width, height, onWordSelect }: Word3DCloudProps) => {
+// 	// Define the onWordClick handler
+// 	const handleWordClick = (word) => {
+// 		if (onWordSelect) {
+// 			onWordSelect(word.text); // Call the callback with the selected word text
+// 		}
+// 	};
 
-export default Word3DCloud;
+// 	return (
+// 		<Box borderRadius="lg" p={4} border={"1px solid"} borderColor={"gray.200"}>
+// 			<WordCloud
+// 				data={words}
+// 				width={1500}
+// 				height={height}
+// 				font="arial"
+// 				fontSize={(d) => d.value}
+// 				padding={5}
+// 				rotate={(d) => d.rotate || 0}
+// 				onWordClick={handleWordClick} // Set the click handler
+// 			/>
+// 		</Box>
+// 	);
+// };
+
+// export default Word3DCloud;
